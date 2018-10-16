c2b03f1c10fd542ce089c2fa64609585448edae0
Liam Owen <l1amow3n5@yahoo.com>
2018-10-16 13:25:18 -0600

    added an array list called response and spooky list and added responses

--- src/model/ChatBot.java
+++ src/model/ChatBot.java
@@ -1,13 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ChatBot
 {
-	private String userName, botName;
+	private String userName, botName, content, joke, currentUser;
+	private ArrayList<String> responseList;
+	private ArrayList<String> spookyList;
+	
 	public ChatBot()
 	{
 		this.userName = "unnamed user";
 		this.botName = "c-bot";
+		this.joke = "Why did the chicken cross the road?";
+		
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList <String>();
+		
+		buildTheLists();
 	}
+	
+	private void buildTheLists()
+	{
+		responseList.add("Hello! How are you?");
+		responseList.add("Goodbye - no more talking!");
+		responseList.add("I dont understand what you said.");
+		responseList.add("I only serve Mother Russia!!");
+		responseList.add("Why do you insult me??");
+		
+	
+	}
+	
+	public String getContent()
+	{
+		return content;
+	}
+	
+	public String getJoke()
+	{
+		return joke;
+	}
+	
+	public String getCurrentUser()
+	{
+		return currentUser;
+	}
+	
 	public String getUserName()
 	{
 		return userName;
@@ -16,6 +54,21 @@ public class ChatBot
 	public String getBotName()
 	{
 		return botName;
+	}
+	
+	public void setContent()
+	{
+		this.content = content;
+	}
+	
+	public void setJoke()
+	{
+		this.joke = joke; 
+	}
+	
+	public void setCurrentUser()
+	{
+		this.currentUser = currentUser;
 	}
 	
 	public void setUserName()
