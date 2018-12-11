57f74d082f3a223486ae88f35270a161e7f9c370
Liam Owen <l1amow3n5@yahoo.com>
2018-12-11 12:19:16 -0700

    made a new class file named IOController and added a public static method

---
+++ src/chat/controller/IOController.java
@@ -0,0 +1,15 @@
+package chat.controller;
+
+import java.util.Calendar;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+
+public class IOController
+{
+	public static void saveText(ChatController app, String path, String textTosave)
+	{
+		
+	}
+}
